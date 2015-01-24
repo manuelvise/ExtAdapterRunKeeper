93432361b7562e514fdefb5386b2c1f1e5ccb008
manuelvise <manuelvise@gmail.com>
2015-01-24 11:42:44 +0100

    methods for retrieving weight and fitness activities of a person from run keeper WS

---
+++ src/introsde/adapter/model/Profile.java
@@ -0,0 +1,83 @@
+package introsde.adapter.model;
+
+public class Profile {
+	private String name;
+	private String location;
+	private String athlete_type;
+	private String gender;
+	private String birthday;
+	private Boolean elite;
+	private String profile;
+	private String small_picture;
+	private String normal_picture;
+	private String medium_picture;
+	private String large_picture;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLocation() {
+		return location;
+	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public String getAthlete_type() {
+		return athlete_type;
+	}
+	public void setAthlete_type(String athlete_type) {
+		this.athlete_type = athlete_type;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	public Boolean getElite() {
+		return elite;
+	}
+	public void setElite(Boolean elite) {
+		this.elite = elite;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public String getSmall_picture() {
+		return small_picture;
+	}
+	public void setSmall_picture(String small_picture) {
+		this.small_picture = small_picture;
+	}
+	public String getNormal_picture() {
+		return normal_picture;
+	}
+	public void setNormal_picture(String normal_picture) {
+		this.normal_picture = normal_picture;
+	}
+	public String getMedium_picture() {
+		return medium_picture;
+	}
+	public void setMedium_picture(String medium_picture) {
+		this.medium_picture = medium_picture;
+	}
+	public String getLarge_picture() {
+		return large_picture;
+	}
+	public void setLarge_picture(String large_picture) {
+		this.large_picture = large_picture;
+	}
+	
+	
+}
